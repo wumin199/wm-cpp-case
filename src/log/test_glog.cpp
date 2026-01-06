6438cefdbf3e1022215d14b69ddaf0e2189a2f97
wumin <wumin@openloong.net>
2026-01-06 09:56:39 +0800

    fix

--- src/log/test_glog.cpp
+++ src/log/test_glog.cpp
@@ -1,8 +1,16 @@
 
+/*
+ * Copyright (c) Min.Wu - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Author: Min.Wu <wumin@126.com>, 2026/01/06
+ */
+
 #include <glog/logging.h>
 #include <gflags/gflags.h>
 
 #include <string>
+#include <filesystem>
 
 int main(int argc, char** argv) {
   google::InitGoogleLogging(argv[0]);
@@ -14,6 +22,13 @@ int main(int argc, char** argv) {
   FLAGS_colorlogtostderr = true;
   FLAGS_max_log_size = 10;
   FLAGS_stop_logging_if_full_disk = true;
+  FLAGS_log_dir = "./logs/xyz_robot_driver_node";
+
+  const std::filesystem::file_status file_status =
+      std::filesystem::status(FLAGS_log_dir);
+  if (!std::filesystem::is_directory(file_status)) {
+    std::filesystem::create_directories(FLAGS_log_dir);
+  }
 
   LOG(INFO) << "This is an info log message.";
   LOG(WARNING) << "This is a warning log message.";
@@ -23,36 +38,32 @@ int main(int argc, char** argv) {
   return 0;
 }
 
-// namespace common {
+class GlogHelper {
+ public:
+  GlogHelper(int argc, char** argv) {
+    google::InitGoogleLogging(argv[0]);
+    google::ParseCommandLineFlags(&argc, &argv, true);
+    google::InstallFailureSignalHandler();
 
-// class GlogHelper {
-//  public:
-//   GlogHelper(int argc, char** argv) {
-//     google::InitGoogleLogging(argv[0]);
-//     google::ParseCommandLineFlags(&argc, &argv, true);
-//     google::InstallFailureSignalHandler();
+    FLAGS_logbuflevel = -1;
+    FLAGS_alsologtostderr = true;
+    FLAGS_colorlogtostderr = true;
+    FLAGS_max_log_size = 10;
+    FLAGS_stop_logging_if_full_disk = true;
+  }
 
-//     FLAGS_logbuflevel = -1;
-//     FLAGS_alsologtostderr = true;
-//     FLAGS_colorlogtostderr = true;
-//     FLAGS_max_log_size = 10;
-//     FLAGS_stop_logging_if_full_disk = true;
-//   }
+  ~GlogHelper() { google::ShutdownGoogleLogging(); }
 
-//   ~GlogHelper() { google::ShutdownGoogleLogging(); }
+  static void SetLogPath(const std::string& destination,
+                         const std::string& extension) {
+    std::string path = destination;
 
-//   void SetLogPath(const std::string& destination,
-//                   const std::string& extension) {
-//     std::string path = destination;
+    if (path.back() != '/') {
+      path += '/';
+    }
 
-//     if (path.back() != '/') {
-//       path += '/';
-//     }
-
-//     google::SetLogDestination(google::INFO, path.c_str());
-//     google::SetLogFilenameExtension((extension + ".").c_str());
-//     LOG(INFO) << "Controller log path: " << path;
-//   }
-// };  // class GlogHelper
-
-// }  // namespace common
+    google::SetLogDestination(google::INFO, path.c_str());
+    google::SetLogFilenameExtension((extension + ".").c_str());
+    LOG(INFO) << "Controller log path: " << path;
+  }
+};
