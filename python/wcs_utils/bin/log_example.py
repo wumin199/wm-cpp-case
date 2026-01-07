fd3192017d07f96b526d873472713268685c2dc1
wumin <wumin@openloong.net>
2026-01-07 16:18:12 +0800

    fix

--- python/wcs_utils/bin/log_example.py
+++ python/wcs_utils/bin/log_example.py
@@ -5,6 +5,7 @@ Proprietary and confidential
 Author: Min.Wu <wumin@126.com>, 2026/01/07
 """
 
+
 import os
 import sys
 
@@ -12,17 +13,18 @@ WCS_PY_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)), "..")
 if os.path.exists(WCS_PY_ROOT):
     sys.path.insert(0, WCS_PY_ROOT)
 
-from wcs_utils.logger.glog import logger, set_log_save_path
 
-# def logger_example():
-#     home_path = os.path.expanduser("~")
-#     set_log_save_path(os.path.join(home_path, "wheel_logs/wcs_utils"))
+from wcs_utils.logger.glog import logger, set_log_save_path  # noqa: E402
 
-#     logger.info("This is an info log message.")
-#     logger.warning("This is a warning log message.")
-#     logger.error("This is an error log message.")
+
+def logger_example():
+    home_path = os.path.expanduser("~")
+    set_log_save_path(os.path.join(home_path, "wheel_logs/wcs_utils"))
+
+    logger.info("This is an info log message.")
+    logger.warning("This is a warning log message.")
+    logger.error("This is an error log message.")
 
 
 if __name__ == "__main__":
-    # logger_example()
-    print("wcs {} logger example".format(WCS_PY_ROOT))
+    logger_example()
