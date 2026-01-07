233e3f80f166acb0ef181b8133fb0a8865872045
wumin <wumin@openloong.net>
2026-01-07 15:27:27 +0800

    update

---
+++ python/wcs_utils/logger/glog.py
@@ -0,0 +1,161 @@
+"""
+Copyright (c) Min.Wu - All Rights Reserved
+Unauthorized copying of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Author: Min.Wu <wumin@126.com>, 2026/01/07
+"""
+
+import datetime
+import logging
+import logging.handlers
+import os
+import sys
+import time
+
+"""A simple Google-style logging wrapper without gflags"""
+
+
+def format_message(record):
+    try:
+        record_message = '%s' % (record.msg % record.args)
+    except TypeError:
+        record_message = record.msg
+    return record_message
+
+
+class GlogColorFormatter(logging.Formatter):
+    LEVEL_MAP = {
+        logging.FATAL: 'F',  # FATAL is alias of CRITICAL
+        logging.ERROR: 'E',
+        logging.WARN: 'W',
+        logging.INFO: 'I',
+        logging.DEBUG: 'D'
+    }
+
+    GREY = "\x1b[38;21m"
+    YELLOW = "\x1b[33;21m"
+    RED = "\x1b[31;21m"
+    BOLD_RED = "\x1b[31;1m"
+    RESET = "\x1b[0m"
+
+    COLOR_MAP = {
+        logging.DEBUG: GREY,
+        logging.INFO: GREY,
+        logging.WARNING: YELLOW,
+        logging.ERROR: RED,
+        logging.CRITICAL: BOLD_RED,
+    }
+
+    def __init__(self, use_color=True):
+        logging.Formatter.__init__(self)
+        self.use_color = use_color
+
+    def format(self, record):
+        try:
+            level = GlogColorFormatter.LEVEL_MAP[record.levelno]
+        except KeyError:
+            level = '?'
+        date = time.localtime(record.created)
+        date_usec = (record.created - int(record.created)) * 1e6
+        if self.use_color:
+            record_message = '%s%c%02d%02d %02d:%02d:%02d.%06d %s %s:%d] %s%s' % (
+                GlogColorFormatter.COLOR_MAP[record.levelno],
+                level, date.tm_mon, date.tm_mday, date.tm_hour, date.tm_min,
+                date.tm_sec, date_usec,
+                record.process if record.process is not None else '?????',
+                record.filename,
+                record.lineno,
+                format_message(record),
+                GlogColorFormatter.RESET)
+        else:
+            record_message = '%c%02d%02d %02d:%02d:%02d.%06d %s %s:%d] %s' % (
+                level, date.tm_mon, date.tm_mday, date.tm_hour, date.tm_min,
+                date.tm_sec, date_usec,
+                record.process if record.process is not None else '?????',
+                record.filename,
+                record.lineno,
+                format_message(record))
+        record.getMessage = lambda: record_message
+        return logging.Formatter.format(self, record)
+
+
+logger = logging.getLogger()
+handler = logging.StreamHandler(sys.stdout)
+handler.flush = sys.stdout.flush
+file_handler = None
+
+
+def set_level(new_level):
+    logger.setLevel(new_level)
+    logger.debug('Log level set to %s', new_level)
+
+
+def set_log_save_path(
+        folder_path=os.path.join(os.path.expanduser("~"), "xyz_log", "xyz_software_tool")):
+    """ set log save path
+
+    Args:
+        folder_path(str): folder path to save
+
+    """
+    # Try to remove file handler
+    global file_handler
+    try:
+        logger.removeHandler(file_handler)
+    except:
+        pass
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    now = None
+    if sys.version_info >= (3, 0):
+        now = datetime.datetime.now().strftime("%Y%m%d-%H%M%S.%f")
+    else:
+        now = datetime.datetime.now().strftime("%Y%m%d-%H%M%S.%f")
+    file_handler = logging.FileHandler(
+        filename=os.path.join(folder_path, "{}.{}".format(os.path.basename(sys.argv[0]), now)))
+    file_handler.setFormatter(GlogColorFormatter(use_color=False))
+    logger.addHandler(file_handler)
+
+
+set_level(logging.INFO)
+
+debug = logging.debug
+info = logging.info
+warning = logging.warning
+warn = logging.warning
+error = logging.error
+exception = logging.exception
+fatal = logging.fatal
+log = logging.log
+
+DEBUG = logging.DEBUG
+INFO = logging.INFO
+WARNING = logging.WARNING
+WARN = logging.WARN
+ERROR = logging.ERROR
+FATAL = logging.FATAL
+
+_level_names = {
+    DEBUG: 'DEBUG',
+    INFO: 'INFO',
+    WARN: 'WARN',
+    ERROR: 'ERROR',
+    FATAL: 'FATAL'
+}
+
+_level_letters = [name[0] for name in _level_names.values()]
+
+GLOG_PREFIX_REGEX = (
+                        r"""
+                        (?x) ^
+                        (?P<severity>[%s])
+                        (?P<month>\d\d)(?P<day>\d\d)\s
+                        (?P<hour>\d\d):(?P<minute>\d\d):(?P<second>\d\d)
+                        \.(?P<microsecond>\d{6})\s+
+                        (?P<process_id>-?\d+)\s
+                        (?P<filename>[a-zA-Z<_][\w._<>-]+):(?P<line>\d+)
+                        \]\s
+                        """) % ''.join(_level_letters)
+"""Regex you can use to parse glog line prefixes."""
+handler.setFormatter(GlogColorFormatter())
+logger.addHandler(handler)
