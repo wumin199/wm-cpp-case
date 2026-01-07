be4efc46723052bc8bec4972a26544ac6d5ceb4d
wumin <wumin@openloong.net>
2026-01-07 09:22:09 +0800

    update

---
+++ src/log/logging.cpp
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) Min.Wu - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Author: Min.Wu <wumin@126.com>, 2026/01/05
+ */
+
+#include "log/logging.h"
